ff1f03977b8d03cc3f04d43ab8e10925dbd00de2
Daniel Trinh <me@daniel.trinh>
2013-05-06 23:22:22 -0700

    add a small standalone spec that uses a local http server for testing

--- test/biz/HttpCrawlerClientSpec.scala
+++ test/biz/HttpCrawlerClientSpec.scala
@@ -1,6 +1,9 @@
 import org.scalatest.{ WordSpec, FunSpec, BeforeAndAfter, BeforeAndAfterAll }
 import org.scalatest.matchers.ShouldMatchers
 import biz.HttpCrawlerClient
+import play.api.libs.ws.WS
+import play.api.test._
+import play.api.test.Helpers._
 import play.core.StaticApplication
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -11,7 +14,7 @@ class HttpCrawlerClientSpec extends WordSpec with BeforeAndAfter with ShouldMatc
   new StaticApplication(new java.io.File("."))
 
   "HttpCrawlerClient" when {
-    ".get" should {
+    ".get(path)" should {
       // TODO: use fake stubbed endpoints instead of real ones
       "fetch a simple https:// url" in {
         val request = HttpCrawlerClient("https://github.com").get("/")
@@ -21,14 +24,19 @@ class HttpCrawlerClientSpec extends WordSpec with BeforeAndAfter with ShouldMatc
       "fetch a simple http:// url" in {
         val client = HttpCrawlerClient("http://www.yahoo.com")
         val request = client.get("/")
-        val res = Await.result(request, 5 seconds)
+        val res = Await.result(request, 5.seconds)
         res.status.value should be === 200
       }
     }
 
-    ".baseDomain" should {
-
+    ".get(path, pipe)" should {
+      "work" in {
+        running(TestServer(3333)) {
+          val client = HttpCrawlerClient("localhost", portOverride = Some(3333))
+          val request = client.get("/", client.bodyOnlyPipeline)
+          val res = Await.result(request, 5.seconds)
+        }
+      }
     }
-
   }
 }
