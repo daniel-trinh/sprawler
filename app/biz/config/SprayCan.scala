851db8b3601a896e398b1e29cfe77e261c82d96a
Daniel Trinh <me@daniel.trinh>
2013-05-06 23:19:48 -0700

    fix broken spray config fetcher

--- app/biz/config/SprayCan.scala
+++ app/biz/config/SprayCan.scala
@@ -5,5 +5,8 @@ import biz.BaseConfig
 object SprayCan extends BaseConfig {
   private val config = base.getConfig("spray.can")
 
-  val userAgent = config.getString("user-agent-header")
+  object Client {
+    private val clientConfig = config.getConfig("client")
+    val userAgent = clientConfig.getString("user-agent-header")
+  }
 }
