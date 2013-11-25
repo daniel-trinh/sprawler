96a0074853040ee358d9ba4dc364410d0cdade1b
Daniel Trinh <me@daniel.trinh>
2013-11-24 18:47:34 -0800

    catch 404s and bad requests and respond with text instead of html

---
+++ examples/deadLinks/app/controllers/Global.scala
@@ -0,0 +1,16 @@
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent.Future
+
+object Global extends play.api.GlobalSettings {
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(NotFound(s"""Not Found @ ${request.toString}
+    |""".stripMargin))
+  }
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    Future.successful(NotFound(
+      s"""|Bad Request @ ${request.toString}
+          |Error @ $error
+          |""".stripMargin))
+  }
+}
